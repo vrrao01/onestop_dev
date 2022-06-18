300acc26d4ec4815d6c34b606b7b322fdb38d36e
Kunalpal215 <83537208+Kunalpal215@users.noreply.github.com>
2022-06-18 08:08:31 +0530

    Update my_fonts.dart

--- lib/globals/my_fonts.dart
+++ lib/globals/my_fonts.dart
@@ -4,17 +4,17 @@ import 'package:onestop_dev/globals/size_config.dart';
 class MyFonts {
   static final String _fontFamily = 'Montserrat';
 
-  static TextStyle get w500 =>
+  static TextStyle get medium =>
       TextStyle(fontFamily: _fontFamily, fontWeight: FontWeight.w500);
-  static TextStyle get w800 =>
+  static TextStyle get extraBold =>
       TextStyle(fontFamily: _fontFamily, fontWeight: FontWeight.w800);
-  static TextStyle get w300 =>
+  static TextStyle get light =>
       TextStyle(fontFamily: _fontFamily, fontWeight: FontWeight.w300);
-  static TextStyle get w400 =>
+  static TextStyle get regular =>
       TextStyle(fontFamily: _fontFamily, fontWeight: FontWeight.w400);
-  static TextStyle get w700 =>
+  static TextStyle get bold =>
       TextStyle(fontFamily: _fontFamily, fontWeight: FontWeight.w700);
-  static TextStyle get w600 =>
+  static TextStyle get med6 =>
       TextStyle(fontFamily: _fontFamily, fontWeight: FontWeight.w600);
 }
 
